cc15e89bbde817701ae67dfb57573d6ee137d374
永夜 <326486210@qq.com>
2020-09-21 20:34:55 +0800

    成功搭建pms和sms工程

---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/mapper/HomeSubjectSpuMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.sms.mapper;
+
+import com.atguigu.gmall.sms.entity.HomeSubjectSpuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 专题商品
+ * 
+ * @author fengge
+ * @email fengge@atguigu.com
+ * @date 2020-09-21 20:19:18
+ */
+@Mapper
+public interface HomeSubjectSpuMapper extends BaseMapper<HomeSubjectSpuEntity> {
+	
+}
