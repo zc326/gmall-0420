aa2812f0610d806c9b8d9e07eab9ddf6509ed021
永夜 <326486210@qq.com>
2020-09-21 20:42:42 +0800

    成功pms和sms+1

--- gmall-sms/src/main/java/com/atguigu/gmall/sms/GmallSmsApplication.java
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/GmallSmsApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.gmall.sms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableSwagger2
 @MapperScan("com.atguigu.gmall.sms.mapper")
+@RefreshScope
 public class GmallSmsApplication {
 
     public static void main(String[] args) {
