b1f094a0ee153571323e882ae61d959ac5104d6e
永夜 <326486210@qq.com>
2020-09-22 08:53:32 +0800

    cors跨域完成

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+public class GmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallGatewayApplication.class, args);
+    }
+
+}
